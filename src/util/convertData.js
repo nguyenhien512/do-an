878f03b6bdb0449e11a3682cf41f346542680445
nkq232 <khanhquanflash@gmail.com>
2023-07-31 23:41:41 +0700

    update statistic feature

--- src/util/convertData.js
+++ src/util/convertData.js
@@ -1,3 +1,16 @@
 export const convertScore = (score) => {
     return score + "-" + (parseInt(score) + 1)
 }
+
+export const convertColor = (level) => {
+    switch (level) {
+        case "LEVEL_1" :
+            return "#FF9999"
+        case "LEVEL_2" :
+            return "#6699FF"
+        case "LEVEL_3" :
+            return "#CC9933"
+        default:
+            return "#CC66FF"
+    }
+}
